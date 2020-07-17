dcfda57d62c9a8b762d3a9b36cc393621cbca567
Luigi <emails@luigi-marino.com>
2020-07-17 10:25:03 +0100

    Scrape necessary data from site

--- covidtracker.py
+++ covidtracker.py
@@ -4,19 +4,21 @@ import requests
 from datetime import date
 
 today = date.today()
-
+emailMessageHeaders = ['Coronavirus Cases', 'Deaths', 'Recovered']
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
 
 r1 = requests.get("https://www.worldometers.info/coronavirus/country/uk/", headers=headers)
 covidData = BeautifulSoup(r1.text, 'html.parser')
 
-for body in covidData.body:
-    if isinstance(body, NavigableString):
-        continue
-    if isinstance(body, Tag):
-        print(body.text)    
+covidData_MainCounter = covidData.find_all('div', attrs={'class' : 'maincounter-number'})
 
-covidData_Raw = ""
+i = 0
+
+#print("Corona Virus Cases")
+for a in covidData_MainCounter:
+    print(emailMessageHeaders[i])
+    print(a.text.strip())
+    i += 1
 
 #covidData_TotalConfirmed
 #covidData_DailyConfirmed
