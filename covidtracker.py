f1044506aeed9ca118d6f9b6bf99e6463719deb9
Luigi <emails@luigi-marino.com>
2020-07-17 11:17:10 +0100

    Comments

--- covidtracker.py
+++ covidtracker.py
@@ -4,36 +4,40 @@ import requests
 from datetime import date
 import csv
 
+#Todays Date
 today = date.today()
+
+#Row Data
 rowEntry = [str(today)]
+#Row Headers
 emailMessageHeaders = ['Coronavirus Cases', 'Deaths', 'Recovered']
+
+#Browser Headers
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
 
+#Get webpage
 r1 = requests.get("https://www.worldometers.info/coronavirus/country/uk/", headers=headers)
 covidData = BeautifulSoup(r1.text, 'html.parser')
 
+#Get Data from webpage
 covidData_MainCounter = covidData.find_all('div', attrs={'class' : 'maincounter-number'})
 
+#Function to remove comma
 def removeComma(b):
     return b.replace(',', '')
 
+#Strip out data and add to row entry
 for a in covidData_MainCounter:
     a = (a.text.strip())
     rowEntry.append(removeComma(a))
 
-print(rowEntry)
 #Field names
 fields = ['Date', 'Total Cases', 'Total Deaths', 'Recovered']
 #File name
 filename = 'data.csv'
 
+#With the file open, write the data
 with open(filename, 'a') as csvfile:
     csvwriter = csv.writer(csvfile)
-    #csvwriter.writerow(fields)
     csvwriter.writerow(rowEntry)
 
-
-#covidData_TotalConfirmed
-#covidData_DailyConfirmed
-#covidData_TotalDeaths
-#covidData_DailyDeaths
